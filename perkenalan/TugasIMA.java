17201b335ab34bcb0671f81dac0f0479619fbda2
raihannaulia <raikodau@gmail.com>
2021-09-30 11:40:48 +0700

    TugasIMA.java

---
+++ perkenalan/TugasIMA.java
@@ -0,0 +1,11 @@
+public class TugasIMA {
+
+	public static void main(String[] args) {
+		double nilai = 4.00d;
+		String nilai2 = "IPK	:";
+		System.out.println("Nama 	:" + "Raihan Aulia Rahman");
+		System.out.println("NIM 	:" + "6706213036");
+		System.out.println("Kelas 	:" + "D3RPLA4505");
+		System.out.println(nilai2+nilai);
+	}
+}
